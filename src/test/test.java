25b4796f28a4602c6dd671422d8f87be41926397
dell <dell@lin>
2019-11-25 16:37:11 +0800

    1

--- src/test/test.java
+++ src/test/test.java
@@ -4,9 +4,9 @@ public class test {
 	
 	public static void main(String[] args) {
 		
-		
-		
 		System.out.println("ssssssssssssss");
+	    System.out.println("1234567899");
+        System.out.println("1234567899");
 	}
 
 }
